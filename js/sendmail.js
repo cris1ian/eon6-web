593d4b91069f39dc459bd061a16c50a3920cb399
emiliopsy <44709555+emiliopsy@users.noreply.github.com>
2020-04-14 11:14:18 -0300

    haciendo el formulario de envio

---
+++ js/sendmail.js
@@ -0,0 +1,26 @@
+//obtener los datos
+console.log("funcionaaa");
+
+
+const sendMail = () => {
+
+    console.log("mandanto el mail");
+
+
+
+
+    // Email.send({
+    //     Host: "smtp.yourisp.com",
+    //     Username: "username",
+    //     Password: "password",
+    //     To: 'them@website.com',
+    //     From: "you@isp.com",
+    //     Subject: "This is the subject",
+    //     Body: "And this is the body"
+    // }).then(
+    //     message => alert(message)
+    // );
+
+
+
+}
