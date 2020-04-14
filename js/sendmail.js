859c7c2311a754c6a6cccf1855695f0b1912b155
emiliopsy <44709555+emiliopsy@users.noreply.github.com>
2020-04-14 11:38:19 -0300

    falta las credenciales del correo

--- js/sendmail.js
+++ js/sendmail.js
@@ -6,21 +6,22 @@ const sendMail = () => {
 
     console.log("mandanto el mail");
 
+    let correo = document.getElementById('formCorreo').value;
+    let nombre = document.getElementById('formNombre').value;
+    let telefono = document.getElementById('formTelefono').value;
+    let mensaje = document.getElementById('formMensaje').value;
 
+    console.log("campos", correo, nombre, telefono, mensaje);
 
-
-    // Email.send({
-    //     Host: "smtp.yourisp.com",
-    //     Username: "username",
-    //     Password: "password",
-    //     To: 'them@website.com',
-    //     From: "you@isp.com",
-    //     Subject: "This is the subject",
-    //     Body: "And this is the body"
-    // }).then(
-    //     message => alert(message)
-    // );
-
+    Email.send({
+        // SecureToken: "C973D7AD-F097-4B95-91F4-40ABC5567812",    
+        To: 'contacto@eon6.tech',
+        From: correo,
+        Subject: "Presupuesto/consulta",
+        Body: `telefono:   ${telefono}` + mensaje
+    }).then((message) => {
+        alert(message);
+    });
 
 
 }
