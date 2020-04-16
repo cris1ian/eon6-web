a033b32a3a40711a5babd7dcb3d58333af03f85c
emiliopsy <44709555+emiliopsy@users.noreply.github.com>
2020-04-16 15:51:04 -0300

    se puede mandar coreos

--- js/sendmail.js
+++ js/sendmail.js
@@ -3,9 +3,29 @@
 var service_id = "default_service";
 var template_id = "template_vIjNb36X";
 
+const enviandoBtn = document.getElementById('enviando-btn');
+const enviarBtn = document.getElementById('enviar-btn');
+const msgOk = document.getElementById('msg-ok');
+const msgErr = document.getElementById('msg-err');
+
+// enviandoBtn.style.visibility = 'hidden';
+enviandoBtn.style.display = 'none';
+enviarBtn.style.display = 'block';
+
+msgOk.style.display = 'none';
+msgErr.style.display = 'none';
+
 const sendMail = () => {
 
     console.log("mandanto el mail");
+
+
+    //muestro el boton el boton    
+    // enviandoBtn.style.visibility = 'visible';
+    enviandoBtn.style.display = 'block';
+    enviarBtn.style.display = 'none';
+    msgOk.style.display = 'none';
+    msgErr.style.display = 'none';
 
     let correo = document.getElementById('formCorreo').value;
     let nombre = document.getElementById('formNombre').value;
@@ -25,8 +45,20 @@ const sendMail = () => {
     emailjs.send(service_id, template_id, template_params)
         .then(function(response) {
             console.log('SUCCESS!', response.status, response.text);
+            document.getElementById('formCorreo').value = "";
+            document.getElementById('formNombre').value = "";
+            document.getElementById('formTelefono').value = "";
+            document.getElementById('formMensaje').value = "";
+            // enviandoBtn.style.visibility = 'hidden';
+            enviandoBtn.style.display = 'none';
+            enviarBtn.style.display = 'block';
+            msgOk.style.display = 'block';
+            msgErr.style.display = 'neno';
         }, function(error) {
             console.log('FAILED...', error);
+            msgErr.style.display = 'block';
+            msgOk.style.display = 'none';
+
         });
 
 
